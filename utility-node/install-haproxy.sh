15a462c2c9682799f633603afe3232d18c5b2ffd
Dan Brown <roooms@gmail.com>
2015-09-14 13:22:23 +0100

    Make HAProxy script non-interactive

--- utility-node/install-haproxy.sh
+++ utility-node/install-haproxy.sh
@@ -4,5 +4,5 @@
 sudo apt-get install haproxy --yes
 sudo cp /etc/haproxy/haproxy.cfg /etc/haproxy/haproxy.cfg.bak
 sudo cp /home/basho/riak-pi-cluster/utility-node/haproxy.cfg /etc/haproxy/haproxy.cfg
-sudo vim /etc/default/haproxy # changed ENABLED=0 to ENABLED=1
+sudo sed -i 's/ENABLED=0/ENABLED=1/g' /etc/default/haproxy
 sudo service haproxy start
